4da3980348759959b0d89036db638c4c8530b341
claudio8140 <tokyp19.aps2a@gmail.com>
2025-04-03 17:01:15 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/claudio-portfolio",
+  // base: "/claudio-portfolio", (pour github deploiement)
+  base: process.env.VITE_BASE_PATH || "/claudio-portfolio",
 })
